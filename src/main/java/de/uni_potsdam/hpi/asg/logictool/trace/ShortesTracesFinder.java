beb5bf76d0363454c0240cba76ec149506ee6983
Norman Kluge <norman.kluge@hpi.de>
2016-08-31 18:54:48 +0200

    refactore ShortestTracesFinder

--- src/main/java/de/uni_potsdam/hpi/asg/logictool/trace/ShortesTracesFinder.java
+++ src/main/java/de/uni_potsdam/hpi/asg/logictool/trace/ShortesTracesFinder.java
@@ -52,73 +52,50 @@ public class ShortesTracesFinder {
     private StateGraph              origsg;
 
     private TraceSimulationStepPool pool;
-    private long                    rmSub  = 0;
-    private long                    rmFall = 0;
+    private List<Transition>        tail;
+    private int                     tmpindex;
+    private boolean                 allcovered;
 
     public ShortesTracesFinder(StateGraph stategraph) {
         this.origsg = stategraph;
+        this.tail = new ArrayList<>();
     }
 
     public SortedSet<TempTrace> findTraces(Signal startSig, Edge startEdge, Signal endSig, Edge endEdge) {
-
         SortedSet<TempTrace> traces = new TreeSet<>(new TraceCmp());
         Deque<TraceSimulationStep> steps = new ArrayDeque<>();
 
         pool = new TraceSimulationStepPool(new TraceSimulationStepFactory());
         pool.setMaxTotal(-1);
-        TraceSimulationStep newStep;
 
-        Set<State> startStates = new HashSet<>();
-        for(State s : origsg.getStates()) {
-            for(Entry<Transition, State> entry2 : s.getNextStates().entrySet()) {
-                if(entry2.getKey().getSignal() == startSig && entry2.getKey().getEdge() == startEdge) {
-                    startStates.add(entry2.getValue());
-                    try {
-                        newStep = pool.borrowObject();
-                    } catch(Exception e) {
-                        e.printStackTrace();
-                        logger.error("Could not borrow object");
-                        return null;
-                    }
-                    newStep.setStart(entry2.getValue());
-                    newStep.setNextState(entry2.getValue());
-                    newStep.getSequence().add(entry2.getKey());
-                    steps.add(newStep);
-                }
-            }
-        }
-
-//        for(State s : startStates) {
-//            try {
-//                newStep = pool.borrowObject();
-//            } catch(Exception e) {
-//                e.printStackTrace();
-//                logger.error("Could not borrow object");
-//                return null;
-//            }
-//            newStep.setStart(s);
-//            newStep.setNextState(s);
-//            newStep.getSequence().add(e)
-//            steps.add(newStep);
-//        }
-
+        Set<State> startStates = findStartStatesAndCreateSteps(startSig, startEdge, steps);
         if(steps.isEmpty()) {
             return null;
         }
 
-//        long stepsEvaledTotal = 0;
+        long stepsEvaledTotal = 0;
         TraceSimulationStep step = null;
         while(!steps.isEmpty()) {
             step = steps.removeLast();
             getNewSteps(step, endSig, endEdge, traces, steps, startStates);
-//            stepsEvaledTotal++;
-//            if(stepsEvaledTotal % 100000 == 0) {
-//                logger.debug("Pool: " + "Created: " + pool.getCreatedCount() + ", Borrowed: " + pool.getBorrowedCount() + ", Returned: " + pool.getReturnedCount() + ", Active: " + pool.getNumActive() + ", Idle: " + pool.getNumIdle());
-//            }
+            stepsEvaledTotal++;
+            if(stepsEvaledTotal % 100000 == 0) {
+                logger.debug("Pool: " + "Created: " + pool.getCreatedCount() + ", Borrowed: " + pool.getBorrowedCount() + ", Returned: " + pool.getReturnedCount() + ", Active: " + pool.getNumActive() + ", Idle: " + pool.getNumIdle());
+            }
         }
         logger.debug("Pool: " + "Created: " + pool.getCreatedCount() + ", Borrowed: " + pool.getBorrowedCount() + ", Returned: " + pool.getReturnedCount() + ", Active: " + pool.getNumActive() + ", Idle: " + pool.getNumIdle());
-        logger.debug("RmSub: " + rmSub + " // RmFall: " + rmFall);
 
+        filterTraces(traces);
+        return traces;
+    }
+
+    private void filterTraces(SortedSet<TempTrace> traces) {
+        /*
+         * Filter traces:
+         * a-b-c-d-e
+         * a-x-b-c-d-e is a subtrace of first and will be removed
+         * (the result is that there are no transitions in the trace which are parallel to the start or end transition) 
+         */
         Queue<TempTrace> checkQ = new LinkedList<>();
         Set<TempTrace> tmpSeq = new HashSet<>();
         checkQ.addAll(traces);
@@ -147,13 +124,31 @@ public class ShortesTracesFinder {
                 }
             }
         }
-
-        return traces;
     }
 
-    private List<Transition> tail = new ArrayList<>();
-    private int              tmpindex;
-    private boolean          allcovered;
+    private Set<State> findStartStatesAndCreateSteps(Signal startSig, Edge startEdge, Deque<TraceSimulationStep> steps) {
+        Set<State> startStates = new HashSet<>();
+        TraceSimulationStep newStep = null;
+        for(State s : origsg.getStates()) {
+            for(Entry<Transition, State> entry2 : s.getNextStates().entrySet()) {
+                if(entry2.getKey().getSignal() == startSig && entry2.getKey().getEdge() == startEdge) {
+                    startStates.add(entry2.getValue());
+                    try {
+                        newStep = pool.borrowObject();
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                        logger.error("Could not borrow object");
+                        return null;
+                    }
+                    newStep.setStart(entry2.getValue());
+                    newStep.setNextState(entry2.getValue());
+                    newStep.getSequence().add(entry2.getKey());
+                    steps.add(newStep);
+                }
+            }
+        }
+        return startStates;
+    }
 
     public int indexOfStart(List<Transition> list, Transition o, int start) {
         for(int i = start; i < list.size(); i++) {
